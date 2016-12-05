6298e2ba42efcfe779f1a5578e3d65b43b1d92bb
SommerNicolas <nicolas.sommer@he-arc.ch>
2016-12-05 11:27:33 +0100

    Test windows

--- Implémentation/NadalinSteve_SommerNicolas.py
+++ Implémentation/NadalinSteve_SommerNicolas.py
@@ -1,7 +1,21 @@
 import pygame
 
+
 def ga_solve(File=None, gui=True, maxTime=0):
     print("ga_Solve")
+    if(File is None):
+        (width, height) = (1800, 900)
+        background_color = (255, 255, 255)
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('Probleme du voyageur de commerce')
+        screen.fill(background_color)
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
