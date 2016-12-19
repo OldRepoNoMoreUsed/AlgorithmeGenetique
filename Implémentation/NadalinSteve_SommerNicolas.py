8ff9b2f4d5eccfced3ca99d50f34bfcbf9367f22
SommerNicolas <nicolas.sommer@he-arc.ch>
2016-12-19 11:14:15 +0100

    Affichage point lecture fichier

--- Implémentation/NadalinSteve_SommerNicolas.py
+++ Implémentation/NadalinSteve_SommerNicolas.py
@@ -1,15 +1,29 @@
 import pygame
 import sys
+import time
 from pygame.locals import KEYDOWN, MOUSEBUTTONDOWN
 
-def drawWindow():
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def addName(self, name):
+        self.name = name
+
+
+def initScreen():
+        (width, height) = (1800, 900)
+        pygame.init()
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('Probleme du voyageur de commerce')
+        font = pygame.font.Font(None, 30)
+        return screen
+
+def drawWindow(screen):
     listeCity = set()
     RED = (255, 0, 0)
-    (width, height) = (1800, 900)
-    pygame.init()
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('Probleme du voyageur de commerce')
-    font = pygame.font.Font(None, 30)
     running = True
     while running:
         for event in pygame.event.get():
@@ -17,35 +31,44 @@ def drawWindow():
                 running = False
             elif event.type == MOUSEBUTTONDOWN:
                 pygame.draw.circle(screen, RED, pygame.mouse.get_pos(), 5)
-                print(pygame.mouse.get_pos())
-                listeCity.add(pygame.mouse.get_pos())
+                x, y = pygame.mouse.get_pos()
+                point = Point(x, y)
+                listeCity.add(point)      #add city
                 pygame.display.flip()
             elif event.type == KEYDOWN:
-                return screen, listeCity
+                return listeCity
     pygame.display.flip()
 
-def drawPath(positions, screen):
-    #for position in positions:
-    #    pygame.draw.circle(screen, (255, 0, 0), position, 5)
-    #    pygame.display.flip()
-    print("Affichage chemin")
+def drawCity(positions, screen):
+    for position in positions:
+        pygame.draw.circle(screen, (255, 0, 0), (int(position.x), int(position.y)), 5)
+        pygame.display.flip()
+        time.sleep(1)
 
-def onClick(event):
-    cx = event.xdata
-    cy = event.ydata
-    print(cy + " " + cx)
+def drawPath(positions, screen):
+    print("Affichage chemin")
+    pygame.display.flip()
+    time.sleep(2)
+
 
 def readFile(File):
+    liste = set()
     with open(File) as f:
         for line in f:
-            print(line)
+            tab = line.split()
+            point = Point(tab[1], tab[2])
+            liste.add(point)
+    return liste
 
 
 def ga_solve(File=None, gui=True, maxTime=0):
+    screen = initScreen()
     if(gui is True):
-        screen, liste = drawWindow()
+        liste = drawWindow(screen)
     else:
-        readFile(File)
+        liste = readFile(File)
+        drawCity(liste, screen)
+    time.sleep(5)
     drawPath(liste, screen)
 
 
